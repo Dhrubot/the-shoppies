0795b5281f34fd6e90905895606da59b4dd8390c
Dhrubot <dhrubo.dh@gmail.com>
2021-01-16 00:08:31 -0500

    Added the info I need on the card have to tinker with the css stuff to give it a nie llok

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,31 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardHeader, CardMedia, CardActions } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    card: {
+        margin: "120px auto 50px",
+        maxWidth: 345,
+        overflow: "visible",
+    },
+    cardMedia: {
+        margin: "-70px auto 0",
+        width: "80%",
+        height: 140,
+        borderRadius: "4px",
+        boxShadow: "0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)",
+        position: "relative",
+        zIndex: 1000
+    },
+    cardHeader: {
+        color: 'black',
+        backgroundColor: 'white'
+    },
+})
 
 const MovieCard = ({ movie, nominateMovie, nominatedMovies }) => {
+
+    const classes = useStyles()
 
     const handleAddingNomination = (nominatedMovies, movie) => {
         nominatedMovies.length === 5 ? <h2>You already chose 5 movies.</h2> : nominateMovie([...nominatedMovies, movie])
@@ -8,10 +33,16 @@ const MovieCard = ({ movie, nominateMovie, nominatedMovies }) => {
     
 
     return (
-        <div>
-            <h4>{movie.Title}</h4>
-            <button onClick={ () => handleAddingNomination(nominatedMovies, movie)} disabled={nominatedMovies.includes(movie)}>Nominate</button>
-        </div>
+        <Card className={classes.card} key={movie.imdbID}>
+            <CardMedia
+                className={classes.cardMedia}
+                image={movie.Poster}
+            />
+            <CardHeader className={classes.cardHeader} title={movie.Title}/>
+            <CardActions>
+                <button onClick={ () => handleAddingNomination(nominatedMovies, movie)} disabled={nominatedMovies.includes(movie)}>Nominate</button>
+            </CardActions>
+        </Card>
     )
 }
 
