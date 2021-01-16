c06f6268a53506e38be98f92cf995ec63b417e47
Dhrubot <dhrubo.dh@gmail.com>
2021-01-16 00:32:35 -0500

    Added the same look for nomination list  looking a little bit bland  will try to fix it in a bit

--- src/components/NominationList.js
+++ src/components/NominationList.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core/styles"
+import { Card } from '@material-ui/core'
+
+
+const useStyles = makeStyles({
+    nominatedListBox: {
+        height: '100vh',
+        overflow: 'auto',
+        margin: 20
+    }
+})
 
 const NominationList = ({ nominated, setNominatedMovies }) => {
+
+    const classes = useStyles()
 
     const handleRemove = (nominatedMovies, movieID) => {
         let newState = nominatedMovies.filter(m => movieID !== m.imdbID)
@@ -8,14 +21,18 @@ const NominationList = ({ nominated, setNominatedMovies }) => {
     }
     
     return (
-        <div>
-            { nominated && nominated.map((movie, idx) => 
+        <>
+        { nominated?.length ? nominated.map((movie, idx) => 
+        <Card className={classes.nominatedListBox} key={idx}>
+            
                 <>
-                <h2 key={idx}>{ movie.Title }</h2>
+                <h2>{ movie.Title }</h2>
                 <button onClick={ () => handleRemove(nominated, movie.imdbID) }>Remove</button>
                 </>
-            )}
-        </div>
+            
+        </Card>
+        ) : <Card className={classes.nominatedListBox}>No movies yet!</Card>}
+        </>
     )
 }
 
