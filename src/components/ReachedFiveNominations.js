5a453f2a48943b68b353a767544c299c9a157c19
Dhrubot <dhrubo.dh@gmail.com>
2021-01-17 13:24:33 -0500

    Added a banner when 5 movies has been added

---
+++ src/components/ReachedFiveNominations.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Collapse, Typography } from "@material-ui/core";
+import { Alert, AlertTitle } from "@material-ui/lab";
+
+const ReachedFiveNominations = ({ show }) => {
+  return (
+    <Box>
+      <Collapse in={ show }>
+        <Alert severity="success">
+          <AlertTitle>
+            <Typography align="left">
+              <strong>Thank You!</strong>
+            </Typography>
+          </AlertTitle>
+          <Typography align="left">You have selected 5 movies.</Typography>
+        </Alert>
+      </Collapse>
+    </Box>
+  );
+};
+
+export default ReachedFiveNominations;
