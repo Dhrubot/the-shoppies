d58c33bf58eb95bcfd1009ba86412a689b27d0e8
Dhrubot <dhrubo.dh@gmail.com>
2021-01-17 08:29:28 -0500

    added a simple footer

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    AppBar,
+    Toolbar,
+    Typography
+} from '@material-ui/core'
+
+const Footer = () => {
+    return (
+        <AppBar position="static" color="transparent">
+            <Toolbar>
+                <Typography variant="body1" >
+                    © 2020 Shoppies
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Footer
+
